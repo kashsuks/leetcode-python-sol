9ce530163ae6f0d07483974d1a93e42708f1ed8a
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2024-08-20 20:20:06 -0400

    Palindrome Number

---
+++ easy/palindromeNumber#9.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        xStr = str(x)
+        arr = []
+        for i in xStr:
+            arr.append(i)
+        if arr == arr[::-1]:
+            return True
+        return False
+    
+#Time Complexity: O(n)
+#Space Complexity: O(n)
